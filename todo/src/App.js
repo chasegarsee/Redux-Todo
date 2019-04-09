adddb1303187d21a38603736e36ee70b411ef1b3
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-04-09 15:10:36 -0500

    MVP completed

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+// import React from "react";
+// import "./App.css";
+// import AddTodo from "";
+// import { VisibleTodoList } from "";
+
+// const App = () => {
+//   <div>
+//     <AddTodo />
+//     <VisibleTodoList />
+//   </div>;
+// };
+
+// export default App;
